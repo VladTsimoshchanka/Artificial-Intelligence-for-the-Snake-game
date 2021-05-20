1b9448b98ac54f68f5161a3865620e6d1817919d
Chief Nelson <chief@Chiefs-MBP.lan>
2021-05-19 19:51:17 -0600

    More progress on non-ML

--- GameProject3/AIProjectCode/NonRandomPlayer.java
+++ GameProject3/AIProjectCode/NonRandomPlayer.java
@@ -4,7 +4,7 @@ public class NonRandomPlayer implements Player
 {
     
     private int my_num;
-    
+    private int enemy_num;
     
     public NonRandomPlayer()
     {
@@ -15,6 +15,16 @@ public class NonRandomPlayer implements Player
     public void begin(GameState init_state, int play_num)
     {
         my_num = play_num;
+        
+        //hard-coded for two players
+        if(my_num == 0)
+        {
+            enemy_num = 1;
+        }
+        else
+        {
+            enemy_num = 0;
+        }
     }
     
     
@@ -22,7 +32,7 @@ public class NonRandomPlayer implements Player
     {
         DirType choice = null;
         
-        //get the our snake object
+        //get the head of our snake object
         Snake the_snake = state.getSnake(my_num);
         HeadPiece the_head = the_snake.getHead();
         
@@ -39,7 +49,13 @@ public class NonRandomPlayer implements Player
         
         //stop the player from colliding with itself or the opponent
         moves = removeBodyCollisions(xPos, yPos, the_snake, moves);
+        moves = removeBodyCollisions(xPos, yPos, state.getSnake(enemy_num), moves);
         
+        //prevent the player from colliding with any walls
+        moves = removeWallCollisions(xPos, yPos, state, moves);
+        
+        //find the nearest food
+        Tup foodPos = getNearestFood(xPos, yPos, state);
         
         return choice;
     }
@@ -72,6 +88,92 @@ public class NonRandomPlayer implements Player
     }
     
     
+    private ArrayList<DirType> removeWallCollisions(int playerX, int playerY, GameState state, ArrayList<DirType> posDirs)
+    {
+        if(northFrom(playerX, playerY).y < 0)
+        {
+            posDirs.remove(DirType.North);
+        }
+        if(eastFrom(playerX, playerY).x > state.max_x)
+        {
+            posDirs.remove(DirType.East);
+        }
+        if(southFrom(playerX, playerY).y > state.max_y)
+        {
+            posDirs.remove(DirType.South);
+        }
+        if(westFrom(playerX, playerY).x < 0)
+        {
+            posDirs.remove(DirType.West);
+        }
+        
+        return posDirs;
+    }
+    
+    
+    private Tup getNearestFood(int playerX, int playerY, GameState state)
+    {
+        Tup nearestFood = new Tup(0, 0);
+        double nearestFdDist = Double.POSITIVE_INFINITY;
+        
+        for(int i = 0; i < state.max_x; i++)
+        {
+            for(int j = 0; j < state.max_y; j++)
+            {
+                if (state.isFood(i, j) && dist(playerX, playerY, i, j) < nearestFdDist)
+                {
+                    nearestFood = new Tup(i, j);
+                    nearestFdDist = dist(playerX, playerY, i, j);
+                }
+            }
+        }
+        
+        return nearestFood;
+    }
+    
+    
+    private DirType moveNearestFood(int playerX, int playerY, Tup food, ArrayList<DirType> posDirs)
+    {
+        
+        //return null if no safe moves
+        if(posDirs.size() == 0)
+        {
+            return null;
+        }
+        
+        
+        
+        DirType current = null;
+        double nearestFdDist = Double.POSITIVE_INFINITY;
+        
+        while(posDirs.size() > 1)
+        {
+            DirType temp = posDirs.get(0);
+            
+            if(temp == DirType.North)
+            {
+                
+            }
+            else if(temp == DirType.East)
+            {
+            
+            }
+            else if(temp == DirType.South)
+            {
+            
+            }
+            else    //west
+            {
+            
+            }
+        }
+
+        
+        return posDirs.get(0);
+    }
+    
+    
+    
     
     //methods and class for handling coords
     class Tup
@@ -97,6 +199,12 @@ public class NonRandomPlayer implements Player
     {
         return new Tup(theX-1, theY);
     }
+    
+    private double dist(int x1, int y1, int x2, int y2)
+    {
+        return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+    }
+    
     
     
     
